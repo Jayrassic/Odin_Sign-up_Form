a4a3bb80f62d85f4103d2e04a274272294da5384
Jason <jasongaglio@gmail.com>
2022-10-31 21:42:12 -0400

    Added validity checker

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const pass = document.querySelector(`#pass`);
 const passCheck = document.querySelector(`#pass-check`);
 const different = document.querySelector(`.diff`);
+// const btn = document.querySelector(`button`);
 
 function checker() {
-    if(pass.value !== passCheck.value){
-        different.textContent = `* Please confirm password`;
-    } else {
-        different.textContent = ``;
+    if (pass.value.length > 1 && passCheck.value.length > 1){
+        if(pass.value !== passCheck.value){
+            different.textContent = `* Please confirm password`;
+            btn.disabled=true;
+            pass.className = `error`
+        } else {
+            different.textContent = ``;
+            btn.disabled=false;
+            pass.className = `normal`
+        }
     }
-}
+};
 
-passCheck.addEventListener(`focusout`, checker);
-pass.addEventListener(`focusout`, checker);
+passCheck.addEventListener(`keyup`, checker);
+pass.addEventListener(`keyup`, checker);
+
+const inputs = document.querySelectorAll(`input`);
+
+for (let i = 0 ; i < inputs.length; i++) {
+    inputs[i].addEventListener(`focusout`, () => {
+    if (inputs[i].validity.valid === false) {
+        inputs[i].className = `error`;
+    } else {
+        inputs[i].className = `normal`;
+    }  
+ })};
